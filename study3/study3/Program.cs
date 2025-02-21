4b94d620d4b5f8203859a4d7f74e9a1c0a9a4dab
박민규 <pumkin8789@gmail.com>
2025-02-21 14:51:05 +0900

    study3

---
+++ study3/study3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string name = "카타리나";
+            char grade = 'S';
+
+
+            Console.WriteLine("캐릭터 : " + name);
+            Console.WriteLine("공격력 : " + att);
+            Console.WriteLine("체력 : " + hp);
+            Console.WriteLine("등급 : " + grade);
+
+        }
+    }
+}
