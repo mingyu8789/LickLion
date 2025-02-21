80b3c8d4abb2cbded30087609eda6eb7786b0900
박민규 <pumkin8789@gmail.com>
2025-02-21 14:50:45 +0900

    study1

---
+++ study1/study1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //출력
+            Console.WriteLine("멋사4기 출력 성공");
+        }
+    }
+}
